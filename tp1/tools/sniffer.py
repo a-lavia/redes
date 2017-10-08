174a9656083e21aefc592ddceecef8bd4e9018de
Joel <joel.e.camera@gmail.com>
2017-10-08 19:59:20 -0300

    sniffer.py toma nombre de red y cantidad de paquetes a tomar

--- tp1/tools/sniffer.py
+++ tp1/tools/sniffer.py
@@ -2,6 +2,14 @@ import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-pcap = sniff(count=100000)
 
-wrpcap("capturaBuffet.pcap", pcap)
+if __name__ == "__main__":
+    if not len(sys.argv) == 3:
+        print "La entrada debe ser:"
+        print "python sniffer.py <nombre-red> <cantidad-paquetes-a-tomar>"
+    else:
+        networkName = sys.argv[1]
+        qttyFrames = sys.argv[2]
+
+        pcap = sniff(count=qttyFrames)
+        wrpcap("captura" + networkName + ".pcap", pcap)
