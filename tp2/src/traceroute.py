7f165e60d19badd95d8eb9e008402bde4791f99b
Joel <joel.e.camera@gmail.com>
2017-11-03 17:17:56 -0300

    traceroute.py terminado

--- tp2/src/traceroute.py
+++ tp2/src/traceroute.py
@@ -9,21 +9,26 @@ import json
 
 def allHops(host):
     cantRespuestas = 30
-    print "Traceroute:", host
     echoReply = False
     ttl=1
     hops = []
-    while (not echoReply) or ttl < 20:
-        dicHop = {}
+    while (not echoReply) and ttl < 20:
+        lst = []
         for i in range(cantRespuestas):
             startTime = time.clock()
-            ans, unans = sr(IP(dst=host,ttl=ttl)/ICMP(), timeout=1) #verbose=0
+            ans, unans = sr(IP(dst=host,ttl=ttl)/ICMP(), timeout=1, verbose=0) #verbose=0
             finishTime = time.clock()
-            if len(ans) != 0:
+            if len(ans) > 0:
                 # guardo la info del hop del ICMP error message y el tiempo en milisegundos
-                lst.append({'ip': ans.res[0][1].src, 'type': ans.res[0][1].type, 'rtt': (finishTime - startTime)*1000})
+                lst.append({'ip': ans.res[0][1].src,
+                            'type': ans.res[0][1].type,
+                            'rtt': (finishTime - startTime)*1000,
+                            'salto_intercontinental': 'false'})
             else:
-                lst.append({'ip': 'null', 'type': 11, 'rtt': 0})
+                lst.append({'ip': 'null',
+                            'type': 11,
+                            'rtt': 0,
+                            'salto_intercontinental': 'null'})
 
         for i in range(len(lst)):
             if lst[i]['type'] == 0:
@@ -41,7 +46,7 @@ def buscoIpQueMasAparece(lstIps):
     lstIpCant = [lstIps.count(ip) for ip in lstIps]
     maxCant = max(lstIpCant)
     idx = lstIpCant.index(maxCant)
-    return lstIpCant[idx]
+    return lstIps[idx]
 
 
 def tracerouteHeuristico(hops):
@@ -66,37 +71,62 @@ def tracerouteHeuristico(hops):
     return traceroute
 
 
-def buscoSaltosIntern(traceroute):
-    media = calcMedia(traceroute)
-    desvEst = calcDesvioEstandar(traceroute, media)
-    lstAbsValDev = calcValorAbsDesv(traceroute, media)
-    tau = calcTau(len(traceroute))
+def buscoSaltosInterc(traceroute):
+    # dic, idx, absolute value of deviation
+    hops = [[traceroute[i], i, 0] for i in range(len(traceroute))]
+    hayOutlier = True
+    while(hayOutlier):
+        hayOutlier = False
+        media = calcMedia(hops)
+        desvEst = calcDesvioEstandar(hops, media)
+        calcValorAbsDesv(hops, media)
+        tau = calcTau(len(hops))
+        tauS = desvEst*tau
+        for i in range(len(hops)):
+            if hops[i][0]['ip'] != 'null' and hops[i][2] > tauS:
+                hayOutlier = True
+                traceroute[hops[i][1]]['salto_intercontinental'] = 'true'
+                hops.pop(i)
+                break
 
 
-def calcMedia(traceroute):
+def calcMedia(hops):
     suma = 0.0
-    for dic in traceroute:
+    for dic,i,_ in hops:
         if dic['ip'] != 'null':
             suma += dic['rtt']
-    return suma / len(traceroute)
+    return suma / len(hops)
 
 
-def calcDesvioEstandar(traceroute, media):
+def calcDesvioEstandar(hops, media):
     suma = 0.0
-    for dic in traceroute:
+    for dic,i,_ in hops:
         if dic['ip'] != 'null':
             suma += (dic['rtt'] - media)**2
-    return (suma/(len(traceroute)-1))**(0.5)
+    return (suma/(len(hops)-1))**(0.5)
 
 
-def calcValorAbsDesv(traceroute, media):
-    return [abs(dic['rtt'] - media) for dic in traceroute if dic['ip'] != 'null']
+def calcValorAbsDesv(hops, media):
+    for i in range(len(hops)):
+        if hops[i][0]['ip'] != 'null':
+            hops[i][2] = abs(hops[i][0]['rtt'] - media)
 
 
 def calcTau(n):
     #Studnt, p<0.05, 2-tail, alpha=0.05
     t = stats.t.ppf(1-0.025, n-2)
     return (t*(n-1)) / ((n**0.5)*((n-2 + t**2)**0.5))
+
+
+def pasarAJson(traceroute):
+    return [json.dumps(hop, indent=4) for hop in traceroute]
+
+
+def imprimirListaJson(lstJson):
+    print '['
+    for j in lstJson:
+        print j
+    print ']'
 
 
 if __name__ == "__main__":
@@ -107,3 +137,5 @@ if __name__ == "__main__":
         hops = allHops(sys.argv[1])
         traceroute = tracerouteHeuristico(hops)
         buscoSaltosInterc(traceroute)
+        lstJson = pasarAJson(traceroute)
+        imprimirListaJson(lstJson)
