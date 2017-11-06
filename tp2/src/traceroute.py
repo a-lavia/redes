7f1ea5567e0f1b3876008640a73cce4fa07c595a
Joel <joel.e.camera@gmail.com>
2017-11-06 14:18:21 -0300

    experimentos + algunos graficos

--- tp2/src/traceroute.py
+++ tp2/src/traceroute.py
@@ -25,7 +25,7 @@ class Traceroute:
         echoReply = False
         ttl=1
         hops = []
-        while (not echoReply) and ttl < 20:
+        while (not echoReply) and ttl < 30:
             lst = []
             for i in range(self.cantRespuestas):
                 startTime = time.clock()
@@ -33,14 +33,16 @@ class Traceroute:
                 finishTime = time.clock()
                 if len(ans) > 0:
                     # guardo la info del hop del ICMP error message y el tiempo en milisegundos
-                    lst.append({'ip': ans.res[0][1].src,
+                    lst.append({'ip_address': ans.res[0][1].src,
                                 'type': ans.res[0][1].type,
+                                'hop_num': ttl,
                                 'rtt': (finishTime - startTime)*1000,
                                 'salto_intercontinental': 'false'})
                 else:
-                    lst.append({'ip': 'null',
-                                'type': 11,
-                                'rtt': 0,
+                    lst.append({'ip_address': 'null',
+                                'type': 'null',
+                                'hop_num': ttl,
+                                'rtt': 'null',
                                 'salto_intercontinental': 'null'})
 
             for i in range(len(lst)):
@@ -55,24 +57,34 @@ class Traceroute:
 
 
     def tracerouteHeuristico(self):
-        # Me quedo con el ip que mas aparece en cada hop
+        # Me quedo con el ip que mas aparece en cada hop pero de rtt menor
         traceroute = []
         for lstHop in self.hops:
             lstIps = []
             for dh in lstHop:
-                if dh['ip'] != 'null':
-                    lstIps.append(dh['ip'])
+                if dh['ip_address'] != 'null':
+                    lstIps.append(dh['ip_address'])
 
             if len(lstIps) == 0:
                 # de este hop no hubo respuesta, agrego una respuesta vacia
                 traceroute.append(lstHop[0])
             else:
                 ip = self.buscoIpQueMasAparece(lstIps)
-                for dh in lstHop:
-                    if dh['ip'] == ip:
-                        traceroute.append(dh)
-                        break
+                # dic vuelve sin el campo type
+                dic = self.menorRTT(lstHop, ip)
+                traceroute.append(dic)
         return traceroute
+
+
+    def menorRTT(self, lstHop, ip):
+        dic = {'rtt': 1000.0}
+        for dh in lstHop:
+            if dh['ip_address'] == ip and dh['rtt'] < dic['rtt']:
+                dic['rtt'] = dh['rtt']
+                dic['ip_address'] = dh['ip_address']
+                dic['hop_num'] = dh['hop_num']
+                dic['salto_intercontinental'] = dh['salto_intercontinental']
+        return dic
 
 
     def buscoIpQueMasAparece(self, lstIps):
@@ -94,7 +106,7 @@ class Traceroute:
             tau = self.calcTau(len(hops))
             tauS = desvEst*tau
             for i in range(len(hops)):
-                if hops[i][0]['ip'] != 'null' and hops[i][2] > tauS:
+                if hops[i][0]['ip_address'] != 'null' and hops[i][2] > tauS:
                     hayOutlier = True
                     self.traceroute[hops[i][1]]['salto_intercontinental'] = 'true'
                     hops.pop(i)
@@ -104,7 +116,7 @@ class Traceroute:
     def calcMedia(self, hops):
         suma = 0.0
         for dic,i,_ in hops:
-            if dic['ip'] != 'null':
+            if dic['ip_address'] != 'null':
                 suma += dic['rtt']
         return suma / len(hops)
 
@@ -112,14 +124,14 @@ class Traceroute:
     def calcDesvioEstandar(self, hops, media):
         suma = 0.0
         for dic,i,_ in hops:
-            if dic['ip'] != 'null':
+            if dic['ip_address'] != 'null':
                 suma += (dic['rtt'] - media)**2
         return (suma/(len(hops)-1))**(0.5)
 
 
     def calcValorAbsDesv(self, hops, media):
         for i in range(len(hops)):
-            if hops[i][0]['ip'] != 'null':
+            if hops[i][0]['ip_address'] != 'null':
                 hops[i][2] = abs(hops[i][0]['rtt'] - media)
 
 
@@ -140,6 +152,62 @@ class Traceroute:
         print ']'
 
 
+    def jsonToFile(self):
+        file = open(self.host + '-json.csv', 'w')
+        file.write('[\n')
+        for x in self.tracerouteJson:
+            file.write(x)
+            file.write('\n')
+        file.write(']')
+        file.close()
+
+
+    def ipsToFile(self):
+        file = open(self.host + '-ips.csv', 'w')
+        for dic in self.traceroute:
+            file.write(dic['ip_address'])
+            file.write('\n')
+        file.close()
+
+
+    def rttToFile(self):
+        file = open(self.host + '-rtt.csv', 'w')
+        for dic in self.traceroute:
+            file.write(str(dic['rtt']))
+            file.write('\n')
+        file.close()
+
+
+    def interJumpToFile(self):
+        file = open(self.host + '-salto.csv', 'w')
+        for dic in self.traceroute:
+            file.write(dic['salto_intercontinental'])
+            file.write('\n')
+        file.close()
+
+
+    def allHopsToFile(self):
+        file = open(self.host + '-allHops.csv', 'w')
+        file.write('ip_address,rtt,salto_intercontinental,type,hop_num')
+        file.write('\n')
+        for lstHop in self.hops:
+            for dic in lstHop:
+                file.write(dic['ip_address'] + "," + \
+                           str(dic['rtt']) + "," + \
+                           dic['salto_intercontinental'] + "," + \
+                           str(dic['type']) + "," + \
+                           str(dic['hop_num']))
+                file.write('\n')
+        file.close()
+
+
+    def allToFile(self):
+        self.jsonToFile()
+        self.ipsToFile()
+        self.rttToFile()
+        self.interJumpToFile()
+        self.allHopsToFile()
+
 
 if __name__ == "__main__":
     if sys.argv[1] == '':
@@ -149,3 +217,5 @@ if __name__ == "__main__":
         traceroute = Traceroute(sys.argv[1])
         traceroute.obtenerTraceroute()
         traceroute.imprimirListaJson()
+        # traceroute.allToFile()
+
